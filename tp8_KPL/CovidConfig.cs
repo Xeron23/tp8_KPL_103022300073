7a33891c09ee04b44eae22ed1f375eee12530dbe
xeron <kaqyword2304@gmail.com>
2025-04-13 19:33:41 +0700

    menyelesaikan program dengan runtime configuration

---
+++ tp8_KPL/CovidConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+ class CovidConfig
+{
+    public string satuan_suhu { get; set; }
+    public int batas_hari_deman { get; set; }
+    public string pesan_ditolak { get; set; }
+    public string pesan_diterima { get; set; }
+
+
+    public  void LoadConfig()
+    {
+        String filePath = "covid_config.json";
+        string json = File.ReadAllText(filePath);
+        CovidConfig config = JsonSerializer.Deserialize<CovidConfig>(json);
+    }
+
+    public void SaveConfig()
+    {
+        String filePath = "covid_config.json";
+        string json = JsonSerializer.Serialize(this, new JsonSerializerOptions { WriteIndented = true });
+        File.WriteAllText(filePath, json);
+    }
+
+    public void UbahSatuan()
+    {
+        if (satuan_suhu == "celcius")
+            satuan_suhu = "fahrenheit";
+        else
+            satuan_suhu = "celcius";
+
+        SaveConfig();
+        Console.WriteLine("Satuan suhu telah diubah menjadi: " + satuan_suhu);
+    }
+}
--- tp8_KPL/Program.cs
+++ tp8_KPL/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        CovidConfig config = new CovidConfig();
+        config.LoadConfig();
+
+        config.UbahSatuan();
+
+        Console.WriteLine($"Berapa suhu badan anda saat ini? Dalam nilai {config.satuan_suhu}");
+        double suhu = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam?");
+        int hari = Convert.ToInt32(Console.ReadLine());
+
+        bool suhuNormal = false;
+
+        if (config.satuan_suhu == "celcius")
+        {
+            suhuNormal = suhu >= 36.5 && suhu <= 37.5;
+        }
+        else if (config.satuan_suhu == "fahrenheit")
+        {
+            suhuNormal = suhu >= 97.7 && suhu <= 99.5;
+        }
+
+        bool hariValid = hari < config.batas_hari_deman;
+
+        if (suhuNormal && hariValid)
+        {
+            Console.WriteLine(config.pesan_diterima);
+        }
+        else
+        {
+            Console.WriteLine(config.pesan_ditolak);
+        }
+    }
+}
